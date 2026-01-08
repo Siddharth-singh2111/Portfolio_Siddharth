7b2572b923bf67979afea6bbf417ddf8c0e73969
Siddharth-singh2111 <siddharth.s23@iiits.in>
2026-01-09 03:00:31 +0530

    changes nextconfig file

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-   eslint: {
+  // @ts-expect-error - The NextConfig type is missing the 'eslint' property definition, but it is valid config.
+  eslint: {
     ignoreDuringBuilds: true,
   },
-  
 };
 
 export default nextConfig;
